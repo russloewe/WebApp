2f292f826cf39b3706e03258141c48420cb432fd
Russell Loewe <russloewe@gmail.com>
2018-11-08 10:53:06 -0800

    added bin folder to git repo

--- load.sh
+++ load.sh
@@ -3,7 +3,7 @@
 load () {
     echo "Copying files from external source"
     FOLDER='sf_webapp'
-    rm -rf `ls | grep -v 'node_modules' | grep -v 'bin' | grep -v '.git' | grep -v`
+    rm -rf `ls | grep -v 'node_modules' | grep -v '.git'`
     cp -R /media/$FOLDER/* ./
 }
 
@@ -16,8 +16,7 @@ while getopts ":hwrl" opt; do
         case ${opt} in
     h|\?)
       echo "-w, --webpack       invoke webpack script"
-      echo "-l, --load          copy files from external to www"
-      echo "-r, --repo          copy files from www to git repo folder"
+      echo "-l, --load          copy files from external to local"
       exit 1
       ;;
     w) webpack ;;
