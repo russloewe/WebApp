02212d012981c49d855fe345644c985331f3a480
Russell Loewe <russloewe@gmail.com>
2018-11-18 20:52:49 -0800

    removed cookie parser from app

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 //set up express-sessions
 app.use(session({secret: "cats",
