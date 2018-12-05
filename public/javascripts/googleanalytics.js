d18919d3d5c2b64ce3fab345eed50e610b5238e0
Russell Loewe <russloewe@gmail.com>
2018-12-04 22:38:12 -0800

    updated google tracker

--- public/javascripts/googleanalytics.js
+++ public/javascripts/googleanalytics.js
@@ -1,9 +1,7 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-44408252-2"></script>
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-44408252-2');
-</script>
+
