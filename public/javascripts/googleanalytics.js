0e873546c591012c62f2bc9303f4a9a3ba053194
Russell Loewe <russloewe@gmail.com>
2018-12-04 22:26:06 -0800

    added google analytics

---
+++ public/javascripts/googleanalytics.js
@@ -0,0 +1,6 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-44408252-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
