8f46e05f3b900df1bfe4f189270f7885a87d2d62
Russell Loewe <russloewe@gmail.com>
2018-12-11 22:52:18 -0800

    updated article component

--- src/react/blog/article.jsx
+++ src/react/blog/article.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {getSimpleId} from "../../api/api.js";
-import {setArticle} from '../../redux/actions';
-import { connect } from "react-redux";
-const mapStateToProps = state => {
-    return{user: state.user};
-};
 
-function Article(props){
+export default function Article(props){
 	    let title;
 	    let datetext;
 	    
 	    
-        const usertype = props.user.usertype;
-        const adminbutton = <a href="/admin">Admin Tools</a>;
-        
+        const usertype = props.usertype;        
         const isAdmin = (type) => {
             if (type != 1){
                 return false;
@@ -37,11 +29,10 @@ function Article(props){
             <h3>{title}</h3>
             {datetext}<br/>
             <div dangerouslySetInnerHTML={{ __html: props.article.text }}></div>
-            
             </div>
-            {isAdmin(props.user.user_type) ? adminbutton : ''}
+
             
        </div>
         )
 }
-export default connect(mapStateToProps)(Article);
+
