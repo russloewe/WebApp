680dd9d4d77ecf7e03707d7280989829d769c4d6
Russell Loewe <russloewe@gmail.com>
2018-12-12 11:50:33 -0800

    added redux actions for setting project, blog titlse, callbacks and parent topic

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -9,8 +9,21 @@ export const userStatus = status => ({type: USER_STATUS, payload: status});
 //ARTICLE ACTIONS
 import {ACTIVE_ARTICLE, ALL_ARTICLES, ALL_ARTICLES_TITLES } from "./action-types";
 export const setArticle = article => ({type: ACTIVE_ARTICLE, payload: article});
-export const allArticles = articles => ({type: ALL_ARTICLES, payload: articles});
-export const allArticlesTitles = articles => ({type: ALL_ARTICLES_TITLES, payload: articles});
+export const Articles = articles => ({type: ALL_ARTICLES, payload: articles});
+
+
+//TITLE ACTIONS
+import { BLOG_TITLES, PROJECT_TITLES } from "./action-types";
+export const setBlogTitles = articles => ({type: BLOG_TITLES, payload: articles});
+export const setProjectTitles = articles => ({type: PROJECT_TITLES, payload: articles});
+
+//NAV ACTIONS
+import { PARENT_TOPIC } from "./action-types";
+export const setParentTopic = topic => ({type: PARENT_TOPIC, payload: topic});
+
+//CALLBACKS
+import { EDIT_ARTICLE_CB } from "./action-types";
+export const setEditArticleCB = cb => ({type: EDIT_ARTICLE_CB, payload: cb});
 
 //Home
 import {SET_HOME} from "./action-types";
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,16 +2,25 @@
 import { UPDATE_USER_LIST, SET_ACTIVE_USER, USER_STATUS} from "./action-types";
 //BLOG ACTIONS
 import { ACTIVE_ARTICLE, ALL_ARTICLES, ALL_ARTICLES_TITLES} from "./action-types";
+//TITLE ACTIONS
+import {PROJECT_TITLES, BLOG_TITLES} from "./action-types";
+//NAV ACTIONS
+import {PARENT_TOPIC} from "./action-types";
 //HOME ACTIONS
 import { SET_HOME } from "./action-types";
+//CALLBACKS
+import { EDIT_ARTICLE_CB } from "./action-types";
 
 const initialState = {
   userList: [],
   activeUser: {},
-  user: {loggedin: false},
+  user: {loggedin: false, isAdmin: false},
   article: {},
   articles: [],
-  articlesTitles: [],
+  editArticleCB: null,
+  blogTitles: [],
+  projectTitles: [],
+  parentTopic: '',
   home: {}
 };
 const rootReducer = (state = initialState, action) => {
@@ -25,6 +34,11 @@ const rootReducer = (state = initialState, action) => {
         return newState;
     case USER_STATUS:
         newState.user = action.payload;
+        if(action.payload.usertype ==1){
+			newState.user.isAdmin = true;
+		}else{
+			newState.user.isAdmin = false;
+		}
         return newState;
     case ACTIVE_ARTICLE:
         newState.article = action.payload;
@@ -32,12 +46,21 @@ const rootReducer = (state = initialState, action) => {
     case ALL_ARTICLES:
         newState.articles = action.payload;
         return newState;
-    case ALL_ARTICLES_TITLES:
-        newState.articlesTitles = action.payload;
+    case PROJECT_TITLES:
+        newState.projectTitles = action.payload;
+        return newState;
+	case BLOG_TITLES:
+        newState.blogTitles = action.payload;
         return newState;
 	case SET_HOME:
         newState.home = action.payload;
         return newState;
+    case PARENT_TOPIC:
+		newState.parentTopic = action.payload;
+		return newState;
+    case EDIT_ARTICLE_CB:
+		newState.editArticleCB = action.payload;
+		return newState;
     default:
       return state;
   }
