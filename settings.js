29fe9d00a86364bf27b13fcc6b33c914e7a6fe10
Russell Loewe <russloewe@gmail.com>
2019-02-01 16:35:55 -0800

    Added settings file for development for local testing

---
+++ settings.js
@@ -0,0 +1,13 @@
+const databaseConfig = {
+    user: 'www',
+    database: 'www',
+    password: 'testpassword',
+    port: 5432
+};
+
+const siteName = 'testsite.com';
+
+module.exports = {
+   databaseConfig: databaseConfig,
+   siteName: siteName
+}
