84ff4d5abf19632523e6995c0967a651be3bf459
russloewe <russloewe@gmail.com>
2019-02-15 18:48:26 -0800

    adding map router

---
+++ routes/map.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var siteName = require('../settings.js').siteName;
+var homeImg = require('../settings.js').homeImg;
+const db = require('../db/db_home.js');
+var ensureAdmin = require('../passport.js').ensureAdmin;
+//set up admin authorized middlware
+const ensureAdminMW = ensureAdmin({redirectTo:'/auth/login?auth=false',
+                                   unauthRedirect: '/auth/unauth',
+                                   userLevel: 1});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('map', { title: siteName, root: 'map',
+                        style: req.style,
+                        footerimage: homeImg,
+                        sitename: siteName});
+});
+
+module.exports = router;
