7f343da72e56442092575125b7584672dcc6bd55
russloewe <russloewe@gmail.com>
2019-05-22 10:38:23 -0700

    e

--- db_scheme.template.sql
+++ db_scheme.template.sql
@@ -1,25 +1,31 @@
 CREATE DATABASE {sql_database_name};
 CREATE USER {sql_username} WITH ENCRYPTED PASSWORD '{sql_user_pass}';
 \c {sql_database_name};
+
+CREATE TABLE siteinfo(
+var VARCHAR UNIQUE,
+val VARCHAR
+);
+
+INSERT INTO siteinfo (var, val) VALUES ('sitename', 'russloewe.com');
+INSERT INTO siteinfo (var, val) VALUES ('url', 'russloewe.com');
+
+
 CREATE TABLE users(
-user_id serial PRIMARY KEY,
-username VARCHAR (50) UNIQUE NOT NULL,
-password VARCHAR (500) NOT NULL,
-email VARCHAR (500) NOT NULL,
-user_type INT NOT NULL,
-created_on TIMESTAMP NOT NULL,
-last_login TIMESTAMP NOT NULL);
+id serial PRIMARY KEY,
+name VARCHAR (50) UNIQUE NOT NULL,
+passwd VARCHAR (500) NOT NULL,
+email VARCHAR (500) NOT NULL);
+
 CREATE TABLE articles(
-article_id serial PRIMARY KEY,
-article_page VARCHAR(50),
+id serial PRIMARY KEY,
+topic VARCHAR(50),
 title VARCHAR(500) NOT NULL,
-author VARCHAR(500),
-published BOOLEAN,
-keywords VARCHAR(500),
-created_on TIMESTAMP NOT NULL,
-thumb_img VARCHAR(500),
+public BOOLEAN,
+created TIMESTAMP NOT NULL,
+img VARCHAR(500),
 description VARCHAR,
-text VARCHAR);
+body VARCHAR);
 
 GRANT ALL PRIVILEGES ON users, articles to {sql_username};
 
