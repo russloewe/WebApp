4e4abc07d2ea959e353d2527bff3c1cb9a61dd98
russloewe <russloewe@gmail.com>
2019-05-24 19:17:59 -0700

    did an add all to clean up all this bs

--- db/db_users.js
+++ db/db_users.js
@@ -1,33 +1,9 @@
 const databaseConfig = require('../settings.js').databaseConfig;
 const pg = require('pg');
-const crypto = require('crypto');
 //postgresql client setup
 const pool = new pg.Pool(databaseConfig);
 console.log("Connecting to Postrges Database with config: ");
 console.log(databaseConfig);
-
-/*
- * user table:
- * user_id: primary key, automatic
- * username: VARCHAR(50) UNIQUE NOT NULL
- * password: VARCHAR(50) NOT NULL
- * passsalt: VARCHAR(20) NOT NULL
- * email:    VARCHAR(355)UNIQUE NOT NULL
- * user_type: INT NOT NULL
- * created_on: TIMESTAMP NOT NULL
- * last_login: TIMESTAMP
- */
- 
-function sqlAddUserFormat(user) {
-   const query = 'INSERT INTO users (username, password, email, user_type, created_on) VALUES';
-   const to_str = [user.username, user.password, user.email];
-   const to_str_quotes = to_str.map(function(ele){
-        return( "'"+ele+"'");
-  });
-  const two = '('+to_str_quotes+', '+user.user_type+', CURRENT_TIMESTAMP);';
-  const final = query + two;
-  return final;
-}; 
 
 function getAllUsers(cb) {
     pool.connect((err, client, donedb) => {
@@ -83,169 +59,12 @@ function findById(userid, cb){
     })
 };
 
-function addUser(user, cb){
-    pool.connect((err, client, donedb) => {
-        if(err){
-			donedb();
-            cb(err, null);
-        }else{
-            let query_str;
-            try{
-                query_str = sqlAddUserFormat(user);
-            }catch(error){
-				donedb();
-                cb(error, null);
-            }
-            client.query(query_str, function(err, result) {
-                donedb();
-                if(err){
-                    cb(err, null);
-                }else{
-                    cb(null, result);
-                };
-            });
-        }
-    });
-};
 
-function updateUserInfo(user, cb){
-    pool.connect((err, client, donedb) => {
-        if(err){
-            donedb();
-            cb(err, null);
-        }else{
-			if(!user.username){
-				donedb();
-				cb(new Error("No username provided"), null)
-			}else if(!user.email){
-				donedb();
-				cb(new Error("No email provided"), null);
-			}else{
-				//eventually change this to only update provided values
-				const intro = "UPDATE users SET ";
-				const name = "username = '" + user.username+"'";
-				const email = "email = '" + user.email+"'";
-				const end = " WHERE user_id ="+user.user_id+";";
-				const query_str = intro + name + ", " + email  + end;
-				client.query(query_str, function(err, res) {
-					donedb();
-					if(err){
-						cb(err, null);
-					}else{
-						cb(null, res);
-					};
-				});
-			}
-        }
-    });
-};
-
-function updateUserType(user, cb){
-    pool.connect((err, client, donedb) => {
-        if(err){
-            donedb();
-            cb(err, null);
-        }else{
-			if(!user.user_type){
-				donedb();
-				cb(new Error("No user_type"), null);
-			}else{
-            //eventually change this to only update provided values
-				const intro = "UPDATE users SET ";
-				const user_type =  "user_type = '" + user.user_type+"'";
-				const end = " WHERE user_id ="+user.user_id+";";
-				const query_str = intro + user_type + end;
-				client.query(query_str, function(err, res) {
-					donedb();
-					if(err){
-						cb(err, null);
-					}else{
-						cb(null, res);
-					};
-				});
-            }
-        }
-    });
-};
-
-function updateUserPassword(user, cb){
-    pool.connect((err, client, donedb) => {
-        if(err){
-            donedb();
-            cb(err, null);
-        }else{
-			if(!user.password){
-				donedb();
-				cb(new Error("No password provided"), null);
-			}else{
-				//eventually change this to only update provided values
-				const intro = "UPDATE users SET ";
-				const pass =  "password = '" + user.password+"'";
-				const end = " WHERE user_id ="+user.user_id+";";
-				const query_str = intro + pass + end;
-				client.query(query_str, function(err, res) {
-					donedb();
-					if(err){
-						cb(err, null);
-					}else{
-						cb(null, res);
-					};
-				});
-			}
-        }
-    });
-};
-
-
-
-function removeUserId(userid, cb){
-    pool.connect((err, client, donedb) => {
-        if(err){
-            cb(err, null);
-            donedb();
-        }else{
-            const query = 'DELETE FROM users WHERE user_id = '+userid+';';
-            client.query(query, function(err, result){
-                donedb();
-                if(err){
-                    cb(err, null);
-                }else{
-                    cb(null, result);
-                }
-            })
-        }
-    })
-};
-
-function removeUserName(username, cb){
-    pool.connect((err, client, donedb) => {
-        if(err){
-            cb(err, null);
-            donedb();
-        }else{
-            const query = "DELETE FROM users WHERE username = '"+username+"';";
-            client.query(query, function(err, result){
-                donedb();
-                if(err){
-                    cb(err, null);
-                }else{
-                    cb(null, result);
-                }
-            })
-        }
-    })
-};
 
 module.exports = {
    findByName: findByName,
    findById: findById,
    getAllUsers: getAllUsers,
-   addUser: addUser,
-   updateUserInfo: updateUserInfo,
-   updateUserPassword: updateUserPassword,
-   updateUserType: updateUserType,
-   removeUserId: removeUserId,
-   removeUserName: removeUserName,
    pool: pool,
    sqlAddUserFormat: sqlAddUserFormat
 }
--- test/db_users_test.js
+++ test/db_users_test.js
@@ -4,41 +4,6 @@ var expect = require("chai").expect;
 var db = require("../db/db_users.js");
 
 describe("Test user database internal api", function(){
-	var test_id;
-    describe('.addUser()', function() {
-        it('Should insert user record to database', function(done) {
-            var user = {username: 'testuser5',
-                    password: 'testpassword',
-                    passsalt: 'testsalt',
-                    email:    'testemail5',
-                    user_type: 3};
-            db.addUser(user, function(err, result) {
-                expect(err).to.be.null;
-                db.findByName('testuser5', function(err2, res) {
-                    expect(err2).to.be.null;
-                    expect(res.username).to.exist;
-                    expect(res.username).to.be.a('string').and.to.equal('testuser5');
-                    test_id = res.user_id;
-                    console.log("Created test user with id: "+test_id);
-                    done();
-                })
-            });
-        }).timeout(1000);
-    });
-    describe('.getAllUsers()', function(){
-        it("Should get a list of all the users", function(done) {
-            db.getAllUsers(function(err, results) {
-                expect(err).to.be.null;
-                expect(results).to.not.be.null;
-                expect(results).to.be.a('array');
-                expect(results[0]).to.have.property('username').and.to.be.a('string');
-                expect(results[0]).to.have.property('password').and.to.be.a('string');
-                expect(results[0]).to.have.property('email').and.to.be.a('string');
-                expect(results[0]).to.have.property('user_id').and.to.be.a('number');
-                done();
-            })
-        })
-    });
     describe('.findByName()', function () {
         it("Should return bob's record", function(done) {
             db.findByName("bob", function(err, result){
@@ -58,72 +23,5 @@ describe("Test user database internal api", function(){
                 done();
             })
         }).timeout(1000);
-    });
-    describe('.updateUserInfo', function(){
-        it('Should update the user email and username', function(done){
-			var rand_email =  Math.random().toString(36).substring(2, 20);
-			var rand_name =  Math.random().toString(36).substring(2, 20);
-			var user = {username: rand_name,
-						user_id: test_id, 
-						email: rand_email};
-			db.updateUserInfo(user, function(err2,res2){
-				expect(err2).to.be.null;
-				db.findById(test_id, function(err3, res3){
-					expect(err3).to.be.null;
-					expect(res3.email).to.equal(rand_email);
-					expect(res3.username).to.equal(rand_name);
-					done();
-				});
-			});
-        }).timeout(1000);
-        it('Should update the user password', function(done){
-            db.findByName('testuser5', function(err, res) {
-                expect(err).to.be.null;
-                var rand_pass =  Math.random().toString(36).substring(2, 20);
-                var user = {password: rand_pass,
-                            user_id: test_id};
-                db.updateUserPassword(user, function(err2,res2){
-                    expect(err2).to.be.null;
-                    db.findById(test_id, function(err3, res3){
-                        expect(err3).to.be.null;
-                        expect(res3.password).to.equal(rand_pass);
-                        done();
-                    });
-                });
-            });
-        }).timeout(1000);
-        it('Should update the user type', function(done){
-			var rand_num =  Math.floor(Math.random() * 10);            
-			var user2 = {user_type: rand_num,
-						user_id: test_id};
-			db.updateUserType(user2, function(err2,res2){
-				expect(err2).to.be.null;
-				db.findById(test_id, function(err3, res3){
-					expect(err3).to.be.null;
-					expect(res3.user_type).to.equal(rand_num);
-					done();
-				});
-			});
-        }).timeout(1000);
-    });
-    describe('.sqlAddUserFormat', function() {
-        it("Should get right formatted string", function(done){
-            const user = {username: 'testuser',
-                          password: 'testpassword',
-                          email:    'testemail',
-                          user_type: 3};
-            const formatted = db.sqlAddUserFormat(user);
-            expect(formatted).to.be.a('string');
-            expect(formatted).to.equal("INSERT INTO users (username, password, email, user_type, created_on) VALUES('testuser','testpassword','testemail', 3, CURRENT_TIMESTAMP);");
-            done();
-        });
-    });
-    describe('.removeUserName()', function() {
-        it('Should remove user using username', function(done) {
-            db.removeUserId(test_id, function(err, res) {
-                expect(err).to.be.null;
-                done();
-            })
-        }).timeout(1000);
-    });
+
 }) 
