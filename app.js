45a47cada127c1a7817b606f9027356dde61c57f
russloewe <russloewe@gmail.com>
2019-06-07 04:41:59 -0700

    fixe type in app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error', { title: "Error Page", root: 'root',
-                        style: req.style,});
+                        style: req.style});
 });
 
 module.exports = app;
