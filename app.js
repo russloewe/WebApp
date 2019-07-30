64d33eb5a8eb7f76b0e6f561731f23faac93b701
russloewe <russloewe@gmail.com>
2019-07-29 21:02:25 -0700

    organized app.js

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var passport = require('./passport').passport;
-var ensureAdmin = require('./passport').ensureAdmin;
-var session = require('express-session');
+const ensureAdmin = require('./passport').ensureAdmin;
 const hashpassword = require('./passport').hashpassword;
-const compression = require('compression');
-const rateLimit = require("express-rate-limit");
 
-//import site settings 
-var siteName = require('./settings.js').siteName;
-
-//import routers
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const pagesRouter = require('./routes/pages');
 var app = express();
+
+
+//set up compression
+const compression = require('compression');
 app.use(compression());
 
 //verify node environment
@@ -26,22 +18,30 @@ console.log("Node env: " + env);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//set up logger
+const logger = require('morgan');
 app.use(logger('dev'));
+
+//set up json 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
 //set up express-sessions
+const session = require('express-session');
 app.use(session({secret: "cats",
                  resave: false,
                  saveUninitialized: true}));
 
 //set up passport
+const passport = require('./passport').passport;
 app.use(passport.initialize());
 app.use(passport.session());
 
 
 //set up admin authorized middlware
+
 const ensureAdminMW = ensureAdmin({redirectTo:'/auth/login?auth=false',
                                    unauthRedirect: '/auth/unauth',
                                    userLevel: 1});
@@ -58,24 +58,33 @@ app.use(function(req, res, next) {
 });
 
 //set up rate limiter
+const rateLimit = require("express-rate-limit");
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100
 });
 
-//set routers
-app.use('/', indexRouter);
-app.use('/', hashpassword);
+//set up authentication router
+const authRouter = require('./routes/auth');
 app.use('/auth/login', apiLimiter);
 app.use('/auth', authRouter);
-app.use('/pages', pagesRouter);
+
+// set up index router
+const indexRouter = require('./routes/index');
+app.use('/', hashpassword);
+app.use('/', indexRouter);
 
 //set static file servers
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/private', ensureAdminMW); // require logged in user
+
+//set up private file server tree 
+app.use('/private', ensureAdmin({redirectTo:'/auth/login?auth=false',
+                                   unauthRedirect: '/auth/unauth',
+                                   userLevel: 1})); // require logged in user
 app.use('/private', express.static(path.join(__dirname, 'private')));
 
 // catch 404 and forward to error handler
+var createError = require('http-errors');
 app.use(function(req, res, next) {
   next(createError(404));
 });
